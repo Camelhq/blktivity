712cba21125e9ee5c06708a12b565781e247467f
Steven Osborne <stevenosborne@Stevens-MacBook-Air.local>
2018-09-19 17:34:48 -0700

    added circleci button

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Blktivity
+
+
+[![CircleCI](https://circleci.com/gh/NikonMcFly/blktivity.svg?style=svg)](https://circleci.com/gh/NikonMcFly/blktivity)
--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,19 @@
-var chai = require('chai')
-var expect = require('chai').expect
-var nock = require('nock')
 
+import { chai, assert } from 'chai'
+import nock from 'nock'
 
+import app from '../server/server.js'
 
 
 describe('testing Url', () => {
-
+  it('this is a test for the signup page', () => {
+    chai.request(app)
+      .get("/")
+      // .end(function(err, res){
+      //   expect(res).to.have.status(200)
+      //   done()
+      // })
+  })
   it('this is a test to a test', () => {
     console.log("this is a test")
   })
